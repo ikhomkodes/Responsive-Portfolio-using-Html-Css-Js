1e2987c78eab0c2caad85a3cfe18348fd24b5c7e
Khom <mgrprinc37@gmail.com>
2022-02-25 18:16:52 +0545

    -close)

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,7 +72,7 @@ tabs.forEach(tab =>{
 })
 
 /*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('services__modal'),
+const modalViews = document.querySelectorAll('.services__modal'),
       modalBtns = document.querySelectorAll('.services__button'),
       modalCloses = document.querySelectorAll('.services__modal-close')
 
